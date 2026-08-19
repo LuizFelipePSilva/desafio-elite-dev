471208cc0944c9b182fc78db5f44fd405afcabff
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-08-19 13:29:39 -0300

    feat(typeorm): add config typeorm and postgres

---
+++ apps/backend/src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default registerAs('typeorm', (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: process.env.NODE_ENV !== 'production',
+}));
