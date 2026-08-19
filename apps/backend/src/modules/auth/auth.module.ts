626612dd9a98449eba93a286800ad1ee4a1e5950
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-08-19 16:49:37 -0300

    remove: remove logic of jwt to module

--- apps/backend/src/modules/auth/auth.module.ts
+++ apps/backend/src/modules/auth/auth.module.ts
@@ -1,25 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './infrastructure/auth.controller';
 import { LoginUseCase } from './application/use-cases/login.use-case';
 import { JwtStrategy } from './infrastructure/strategies/jwt.strategy';
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.getOrThrow('JWT_SECRET'),
-        signOptions: { expiresIn: config.getOrThrow('JWT_EXPIRES_IN') },
-      }),
-    }),
-  ],
+  imports: [UsersModule, PassportModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [LoginUseCase, JwtStrategy],
 })
