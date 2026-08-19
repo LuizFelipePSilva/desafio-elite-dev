ef39250e9c54427fe3bf518cabc1e77f510507ff
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-08-19 14:26:34 -0300

    feat(jwt): implement jwt interface, guard and strategy

---
+++ apps/backend/src/modules/auth/infrastructure/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+// modules/auth/infrastructure/strategies/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import {
+  JwtPayload,
+  AuthenticatedUser,
+} from '../../domain/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.getOrThrow('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: JwtPayload): AuthenticatedUser {
+    return { id: payload.sub, role: payload.role };
+  }
+}
