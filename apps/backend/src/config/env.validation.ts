357f931f2ec72250b5357fd13ae3384f03d12ff7
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-08-19 16:50:57 -0300

    refactor: switch names of jwt secret

--- apps/backend/src/config/env.validation.ts
+++ apps/backend/src/config/env.validation.ts
@@ -19,8 +19,12 @@ class EnvironmentVariables {
   @IsString() REDIS_LOCK_PASS: string;
 
   @IsString() PEPPER: string;
-  @IsString() JWT_SECRET: string;
-  @IsString() JWT_EXPIRES_IN: string;
+
+  @IsString() JWT_ACCESS_SECRET: string;
+  @IsString() JWT_ACCESS_EXPIRES_IN: string;
+
+  @IsString() JWT_REFRESH_SECRET: string;
+  @IsString() JWT_REFRESH_EXPIRES_IN: string;
 }
 
 export function validate(config: Record<string, unknown>) {
