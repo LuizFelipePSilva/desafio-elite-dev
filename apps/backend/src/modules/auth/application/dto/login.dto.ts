bed5c535b0363b5821fee121c6d585ab79f0a498
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-08-19 14:25:17 -0300

    feat(login): implement loginDto

---
+++ apps/backend/src/modules/auth/application/dto/login.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class LoginDto {
+  @ApiProperty({
+    example: 'john@example.com',
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: 'secret123',
+    minLength: 8,
+  })
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
